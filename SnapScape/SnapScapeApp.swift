f90c34eb6e7f0cde46250c999dabf487eeb0b33a
Jigar Thakkar <ijigarthakkar@gmail.com>
2023-09-27 19:58:16 +1000

    Initial Commit

---
+++ SnapScape/SnapScapeApp.swift
@@ -0,0 +1,32 @@
+//
+//  SnapScapeApp.swift
+//  SnapScape
+//
+//  Created by Jigar Thakkar on 27/9/2023.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct SnapScapeApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
